652050f07c340db9e2208c57bfda6b30f526cde8
Subham Singh <subhamspy007@gmail.com>
2018-05-21 17:10:07 +0530

    Add files via upload

---
+++ gender-classifier/gender-classifier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 21 16:58:47 2018
+
+@author: subham
+"""
+
+from sklearn import tree
+
+#[height,weight,shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+#classifier
+clf= tree.DecisionTreeClassifier()
+clf= clf.fit(X,Y)
+clf= clf.predict([[190,70,43]])
+print(clf)
